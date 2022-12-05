760bd8b478bc1eb8b30cce1294ca182db8131440
Lucie Tesarova <lucka2121@gmail.com>
2022-12-05 17:18:06 +0100

    creating test for login page with combinations of credentialss for login page with combinations of credentionals

--- cypress/e2e/login_verification.cy.js
+++ cypress/e2e/login_verification.cy.js
@@ -13,5 +13,33 @@ it('TS_1_TC_001_Login with valid username and valid password', function () {
   loginPage.enterUsername(this.data.Login.username)
   loginPage.enterPassword(this.data.Login.password)
   loginPage.clickLogin()
+  cy.get('.oxd-alert').should("not.exist")
   cy.url().should('eq', this.data.Adress.homepage)
 });
+
+it('TS_1_TC_002_Login with valid username and invalid password', function(){
+  loginPage.enterUsername(this.data.Login.username)
+  loginPage.enterPassword(this.data.Login.invalidPassword)
+  loginPage.clickLogin()
+  cy.get('.oxd-alert').should("be.visible")
+  cy.get('.oxd-alert-content > .oxd-text').should("contain", "Invalid credentials")
+  cy.url().should('eq', this.data.Adress.loginPage)
+})
+
+it('TS_1_TC_003_Login with invalid username and valid password', function(){
+  loginPage.enterUsername(this.data.Login.invalidUsername)
+  loginPage.enterPassword(this.data.Login.password)
+  loginPage.clickLogin()
+  cy.get('.oxd-alert').should("be.visible")
+  cy.get('.oxd-alert-content > .oxd-text').should("contain", "Invalid credentials")
+  cy.url().should('eq', this.data.Adress.loginPage)
+})
+
+it('TS_1_TC_004_Login with invalid username and invalid password', function(){
+  loginPage.enterUsername(this.data.Login.invalidUsername)
+  loginPage.enterPassword(this.data.Login.invalidPassword)
+  loginPage.clickLogin()
+  cy.get('.oxd-alert').should("be.visible")
+  cy.get('.oxd-alert-content > .oxd-text').should("contain", "Invalid credentials")
+  cy.url().should('eq', this.data.Adress.loginPage)
+})
