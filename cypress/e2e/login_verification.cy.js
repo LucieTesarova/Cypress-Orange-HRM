cf2a2d630b56c3ebfff97a6df74d1de85d057d9e
Lucie Tesarova <lucka2121@gmail.com>
2022-12-06 18:59:38 +0100

    creating test cases for login with blank space

--- cypress/e2e/login_verification.cy.js
+++ cypress/e2e/login_verification.cy.js
@@ -43,3 +43,28 @@ it('TS_1_TC_004_Login with invalid username and invalid password', function(){
   cy.get('.oxd-alert-content > .oxd-text').should("contain", "Invalid credentials")
   cy.url().should('eq', this.data.Adress.loginPage)
 })
+
+it("TS_1_TC_005_Login with blank username and blank password", function(){
+  loginPage.enterUsername(" ")
+  loginPage.enterPassword(" ")
+  loginPage.clickLogin()
+  cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should("be.visible").and("contain", "Required")
+  cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should("be.visible").and("contain", "Required")
+  cy.url().should('eq', this.data.Adress.loginPage)
+})
+
+it("TS_1_TC_006_Login with blank username and valid password", function(){
+  loginPage.enterUsername(" ")
+  loginPage.enterPassword(this.data.Login.password)
+  loginPage.clickLogin()
+  cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should("be.visible").and("contain", "Required")
+  cy.url().should('eq', this.data.Adress.loginPage)
+})
+
+it("TS_1_TC_007_Login with valid username and blank password", function(){
+  loginPage.enterUsername(this.data.Login.username)
+  loginPage.enterPassword(" ")
+  loginPage.clickLogin()
+  cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should("be.visible").and("contain", "Required")
+  cy.url().should('eq', this.data.Adress.loginPage)
+})
